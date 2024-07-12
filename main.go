a52ee843975ef01da3cb8737e27214e74b821007
lijianhua <xiaomu_0755@163.com>
2024-07-12 16:09:15 +0800

    测试

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"main_gin_go/sqlchecker"
 	"net/http"
 	"reflect"
@@ -46,45 +45,11 @@ type InnerObject struct {
 	Arr []int `json:"arr"`
 }
 
-func initGo() {
-	arr := []int{1, 2, 3}
-	log.Printf("arr: %v", arr)
-	log.Println(arr)
-	var iface interface{} = []string{"a", "b", "c"}
-	slice := []string{}
-	if ifacCopy, ok := iface.([]string); ok {
-		slice = append(slice, ifacCopy...)
-	}
-	log.Printf("slice: %v", slice)
-	//cap 使用
-	ifaceValue := iface.([]string) // 先进行类型断言，将iface转换为具体的类型
-	if ifaceValue != nil {         // 检查断言后的值是否为空
-		log.Println("ifaceValue的容量长度为：", cap(ifaceValue)) // 只有在断言成功后，才能使用cap函数
-	} else {
-		log.Println("iface is nil") // 接口变量为空
-	}
-	arr_ := make([]int, len(arr))
-	copy(arr_, arr)
-
-	log.Printf("arr_ copy的内容为: %v,", arr_)
-
-	//创建复数
-	var complexNumber complex64 = 1 + 2i
-	conmpInt := complex(float64(2), float64(3))
-	log.Printf("获取值拿到复数：complexNumber: %v, conmpInt: %v", complexNumber, conmpInt)
-
-}
-
 func main() {
 	router := gin.Default()
 
-	// 注册中间件
+	// 注册中间件  防护示例
 	router.Use(sqlchecker.SQLInjectionChecker)
-	// withoutSQLCheckGroup := router.Group("/aab", func(c *gin.Context) {
-	// 	c.Next() // 确保继续执行组内的其他处理器
-	// })
-	// withoutSQLCheckGroup.Use(gin.Logger()) // 示例：可以在这里添加针对这个组的局部中间件
-	// withoutSQLCheckGroup.Use(gin.Recovery())
 
 	// 在此路由组内定义的路由将不受全局中间件的影响
 	router.GET("/test-get1", func(c *gin.Context) {
@@ -108,14 +73,10 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"message": "GET request successful"})
 	})
 	router.GET("/get/login", func(c *gin.Context) {
-		// 由于标准的 GET 请求不应该有请求体，Gin 默认也不会读取 GET 请求的请求体。
-		// 为了演示目的，我们尝试手动读取请求体，这在实际应用中可能不适用或需要特别配置。
+
 		user := c.Query("user")
 		pwd := c.Query("pwd")
 		fmt.Printf("Received user: %s, password: %s\n", user, pwd)
-
-		// 这里处理 user 和 pwd
-		// ...
 
 		c.JSON(http.StatusOK, gin.H{"message": "Received GET request with JSON body", "data": pwd})
 	})
@@ -143,19 +104,12 @@ func main() {
 			return
 		}
 
-		// 打印UserCredentials的类型信息
-		fmt.Printf("Type of creds: %s\n", reflect.TypeOf(creds))
-
-		// 打印接收到的用户名和密码
-		fmt.Printf("Received user: %s, password: %s\n", creds.User, creds.Pwd)
-
-		// 同时，你也可以直接使用反射来访问和打印结构体字段的值
 		val := reflect.ValueOf(creds)
 		fmt.Printf("User via reflection: %s, Password via reflection: %s\n", val.FieldByName("User").String(), val.FieldByName("Pwd").String())
 
 		c.JSON(http.StatusOK, gin.H{"message": "POST request successful with user:", "user": creds.User})
 
 	})
-	initGo()
+
 	router.Run(":50269")
 }
